bba88fbb7c54120d2adc5285aa9810480d232aa7
ericjiji3 <eric.ji3333@gmail.com>
2024-09-14 19:22:56 -0400

    djfbdj

--- src/app/page.js
+++ src/app/page.js
@@ -6,14 +6,16 @@ import Intro from '../../public/images/intro.gif';
 // import IntroVid from '../../public/images/introVid.mov';
 import { motion,AnimatePresence } from "framer-motion";
 import { useState } from "react";
+import IntroVideo from "@/components/IntroVideo";
 
 export default function Home() {
-  
+  const [click, setClick] = useState(false);
 
   return (
     <div className="absolute top-0 px-[1rem] md:px-[3.75rem] pt-[5.5rem] md:pt-[6.88rem] pb-[1.5rem] md:pb-[2.5rem] flex flex-col-reverse md:flex-row flex-wrap gap-x-[10%] w-full h-full justify-between">
-
-      <div className={"w-full md:w-[45%] h-[60%] md:h-full flex flex-col opacity-1"}>
+        <IntroVideo/>
+      
+      <div className={click ? "w-full md:w-[45%] h-[60%] md:h-full flex flex-col opacity-1" :"w-full md:w-[45%] h-[60%] md:h-full flex flex-col opacity-1"}>
         <div className="slide mb-[1rem] md:mb-[5rem] relative">
           <div className="relative overflow-hidden">
             <h1>PHORIN COLLECTIVE</h1>
@@ -34,7 +36,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className={"relative w-full md:w-[45%] h-[40%] md:h-full overflow-hidden opacity-1"}>
+      <div className={click ? "relative w-full md:w-[45%] h-[40%] md:h-full overflow-hidden opacity-1" :"relative w-full md:w-[45%] h-[40%] md:h-full overflow-hidden opacity-1"}>
         <div className="absolute top-0 left-0 block w-full h-full bg-white animate-slideY"></div>
         <Image src={HomeImage} className="w-full h-full object-cover" width={500} alt="oops"/>
       </div>
